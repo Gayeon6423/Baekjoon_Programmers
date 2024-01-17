54ed6acccaa60f0c4fc5732519668c9adc2a80b6
GAYEON JUNG <113704015+Gayeon6423@users.noreply.github.com>
2024-01-17 11:28:17 +0900

    Update 최소， 최대.py

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,5 +1,19 @@
+# 풀이1
 import sys
 N = int(sys.stdin.readline())
 li = list(map(int, sys.stdin.readline().split()))
 
 print(min(li), max(li))
+
+# 풀이2
+N = int(input())
+li = list(map(int,input().split()))
+max = li[0]
+min = li[0]
+
+for i in li[1:]:
+    if i > max:
+        max = i
+    elif i < min:
+        min = i
+print(max, min)
