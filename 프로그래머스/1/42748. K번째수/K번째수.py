12a17c33894686d3f1ed4b52dae70d06f3e94dcd
GAYEON JUNG <113704015+Gayeon6423@users.noreply.github.com>
2024-07-25 11:47:45 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,6 @@
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        ans = sorted(array[command[0]-1:command[1]])[command[2]-1]
+        answer.append(ans)
+    return answer
