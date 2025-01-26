eded0a854833dfb7539a3fa46276a92e2a22a317
JUNG GAYEON <113704015+Gayeon6423@users.noreply.github.com>
2025-01-26 14:05:13 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,26 +1,24 @@
 def solution(name):
-    answer = 0
+    ans = 0
     length = len(name)
-    updown_move = 0
-    leftright_move = 0
-    # 상하 이동 최소 횟수
-    for char in name:
-        up_distance = ord(char) - ord('A') # 조이스틱 위로 이동
-        down_distance = ord('Z') - ord(char) + 1 # 조이스틱 아래로 이동
-        updown_move += min(up_distance,down_distance)
-    
-    
-    # 좌우 이동 최소 횟수 계산
-    min_leftright_move = length - 1
-    for index in range(length):
+    # 알파벳 변경 횟수
+    up_down_move = 0
+    # 커서 이동 횟수
+    left_right_move = length - 1
+
+    for index, spell in enumerate(name):
+        # 알파벳 변경(위로 이동)
+        up = ord(spell) - ord('A')
+        # 알파벳 변경(아래로 이동)
+        down = ord('Z') - ord(spell) + 1
+        up_down_move += min(up,down)
+        # 해당 알파벳 다음부터 연속된 A 문자열 찾기
         next_index = index + 1
-        while next_index < length and name[next_index] == 'A': # 다음 문자가 'A'일때
+        while next_index < length and name[next_index] == 'A':
             next_index += 1
-        min_leftright_move  = min(min_leftright_move, # 우측으로만 이동
-                                  2 * index + length - next_index,
-                                  index + 2 * (length - next_index))
-
-    leftright_move += min_leftright_move
-    answer = updown_move + leftright_move
-
-    return answer
+        left_right_move = min([left_right_move, # 이전 커서 이동 값(->)
+                            2*index + length - next_index, # 연속된 A의 왼쪽 시작(->,<-,<-)
+                            index + 2*(length - next_index)  # 연속된 A의 오른쪽 시작(<-,->,->)
+                                        ])
+    ans = up_down_move + left_right_move
+    return ans
