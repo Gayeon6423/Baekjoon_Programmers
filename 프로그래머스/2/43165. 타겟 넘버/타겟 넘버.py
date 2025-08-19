2ddd2b41ce16aeed7c4c924a02db6fda2ce90941
JUNG GAYEON <113704015+Gayeon6423@users.noreply.github.com>
2025-08-19 10:43:15 +0900

    [level 2] Title: 타겟 넘버, Time: 1310.49 ms, Memory: 98.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,36 +1,37 @@
 # BFS 구현(큐 사용)
 # index를 1씩 키우면서 1개는 +, 1개는 -해주며 deque에 append
-# solution1
+
 from collections import deque
 def solution(numbers,target):
     count = 0
     quque = deque()
     quque.append((0,0))
     while quque:
-        cur_sum, index = quque.popleft()
+        index, cur_sum  = quque.popleft()
         
         if index == len(numbers):
             if cur_sum == target:
                 count += 1
         else:
             cur_num = numbers[index]
-            quque.append((cur_sum + cur_num,index + 1))
-            quque.append((cur_sum - cur_num, index + 1))
+            quque.append((index + 1, cur_sum + cur_num))
+            quque.append((index + 1, cur_sum - cur_num))
     return count
 
-# solution2
-def solution(numbers, target):
-    leaves = [0] # 모든 결과
-    count = 0
-    for num in numbers:
-        temp = []
-    # 자손 노드
-        for leaf in leaves:
-            temp.append(leaf+num) # 더하는 경우
-            temp.append(leaf-num) # 빼는 경우
-        leaves = temp
-    # 모든 경우의 수 계산 후 target과 같은지 확인
-    for leaf in leaves:
-        if leaf == target:
-            count += 1
-    return count
+
+
+# from collections import deque
+# def solution(numbers,target):
+#     cnt = 0
+#     quque = deque()
+#     quque.append((0,0))
+#     while quque:
+#         idx, cur_sum = quque.popleft()
+#         if idx == len(numbers):
+#             if cur_sum == target:
+#                 cnt += 1
+#         else:
+#             cur_num = numbers[idx]
+#             quque.append((idx+1, cur_num + cur_sum))
+#             quque.append((idx+1, cur_num - cur_sum))
+#     return cnt
