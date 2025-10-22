f086bc66350181e30dda8d0125c24890e098c44f
JUNG GAYEON <113704015+Gayeon6423@users.noreply.github.com>
2025-10-22 22:47:08 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -2,18 +2,49 @@
 # 종류별로 의상을 나눠 저장한 딕셔너리 closet 가지고 경우의 수 계산
 # A의 종류가 N개, B의 종류가 M개 일 때 가능한 모든 경우의 수는 (N+1)(M+1)로 구할 수 있다. 
 
+# def solution(clothes):
+#     # 각 종류별 가진 의상을 저장(카테고리:[이름,이름,...])
+#     closet = {}
+#     for name, cat in clothes:
+#         if cat not in closet:
+#             closet[cat] = []
+#         closet[cat].append(name)
+#     # A의 종류가 N개, B의 종류가 M개 일 때 가능한 모든 경우의 수 (N+1)(M+1)
+#     # (N+1)(M+1) = NM + N + M + 1
+#     ans = 1
+#     for _, value in closet.items():
+#         ans = ans * (len(value) + 1)
+#     # 아무것도 안 입는 경우를 제외하기 위해 -1
+#     ans  = ans - 1
+#     return ans
+
 def solution(clothes):
-    # 각 종류별 가진 의상을 저장(카테고리:[이름,이름,...])
     closet = {}
     for name, cat in clothes:
         if cat not in closet:
             closet[cat] = []
         closet[cat].append(name)
-    # A의 종류가 N개, B의 종류가 M개 일 때 가능한 모든 경우의 수 (N+1)(M+1)
-    # (N+1)(M+1) = NM + N + M + 1
     ans = 1
     for _, value in closet.items():
         ans = ans * (len(value) + 1)
-    # 아무것도 안 입는 경우를 제외하기 위해 -1
-    ans  = ans - 1
+    ans = ans - 1
+    return ans
+
+def solution(clothes):
+    # 옷장 해시 테이블
+    closet = {}
+    for name, cat in clothes:
+        # print(name, cat)
+        if cat not in closet:           
+            # 카테고리가 옷장 테이블에 없다면 카테고리 새롭게 추가
+            closet[cat] = []
+        # 카테고리가 항목 추가
+        closet[cat].append(name)
+    # 옷 조합 기본 수
+    ans = 1
+    # 조합의 수 = (n+1) * (m+1) * (z+1) * '''
+    # 조합의 수 = 조합의 수 - 1(전체)
+    for key,value in closet.items():
+        ans = ans * (len(value)+1)
+    ans = ans - 1
     return ans
